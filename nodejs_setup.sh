81065fa494bb01fdd886f1608a1b8d7ae3320d26
Paulmicha <paul.michalet@gmail.com>
2013-02-27 23:29:11 +0100

    Adding nodejs_setup.sh

---
+++ nodejs_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -*- coding: UTF8 -*-
+
+##
+#   NodeJS setup
+#   Debian 6 ("Squeeze")
+#   
+#   Sources :
+#   http://unix.stackexchange.com/questions/35679/whats-the-currently-recommended-way-to-install-node-js-on-debian
+#
+
+apt-get install python g++ curl libssl-dev
+mkdir /tmp/nodejs && cd /tmp/nodejs
+wget http://nodejs.org/dist/node-latest.tar.gz
+tar xzvf node-latest.tar.gz && cd node-v*
+./configure
+make
+make test
+make install
