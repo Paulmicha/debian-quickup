635b77649a1f8a795ec8c82433dfa1fc5ab0eb58
Paulmicha <paul.michalet@gmail.com>
2013-09-18 20:55:24 +0200

    Old modif uncommitted on legacy node.js install

--- nodejs_setup.sh
+++ nodejs_setup.sh
@@ -9,7 +9,7 @@
 #   http://unix.stackexchange.com/questions/35679/whats-the-currently-recommended-way-to-install-node-js-on-debian
 #
 
-apt-get install python g++ curl libssl-dev
+apt-get install python g++ curl libssl-dev -y
 mkdir /tmp/nodejs && cd /tmp/nodejs
 wget http://nodejs.org/dist/node-latest.tar.gz
 tar xzvf node-latest.tar.gz && cd node-v*
